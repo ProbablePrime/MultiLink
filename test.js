b9dcf57c6e80263e722129a1642d3005c98d3193
Richard Fox <richard.fox@ahref.co.uk>
2016-01-02 05:45:05 +0000

    These are now in actual tests

--- test.js
+++
@@ -1,7 +0,0 @@
-var utils = require('./dist/Utils.js');
-
-var result = utils.flattenBeamMessage([{ type: 'text', data: 'sdfsdfds' },{ type: 'text', data: 'sdfsdfds' }]);
-
-console.log(result);
-
-
